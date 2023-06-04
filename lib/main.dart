96b75a91ef5b3f2730254335dce76a7d7bb63f89
Jane-Mwangi <janiegrace097@gmail.com>
2023-06-04 10:57:41 +0300

    Create function to allow change in state once the TextButton is pressed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.cyanAccent,
         appBar: AppBar(
           title: Text('Ask Me Anything'),
-          backgroundColor: Colors.cyan,
+          backgroundColor: Colors.cyanAccent,
         ),
-       body: AskMeAnythingPage(),
+        body: AskMeAnythingPage(),
       ),
     ),
   );
@@ -38,13 +39,29 @@ class AskMeAnythingPage extends StatefulWidget {
 }
 
 class _AskMeAnythingPageState extends State<AskMeAnythingPage> {
+  int yesAnswer = 1;
+  int noAnswer = 2;
+  int askAgainLaterAnswer = 3;
+  int theAnswerIsYesAnswer = 4;
+  int iHaveNoIdeaAnswer = 5;
+
+  void changeAnswer() {
+    setState(() {
+      yesAnswer = Random().nextInt(5) + 1;
+      noAnswer = Random().nextInt(5) + 1;
+      askAgainLaterAnswer = Random().nextInt(5) + 1;
+      theAnswerIsYesAnswer = Random().nextInt(5) + 1;
+      iHaveNoIdeaAnswer = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextButton(
-        onPressed: (){
-
-        }, child: Image.asset('images/ball1.png'),
+        onPressed: () {
+          changeAnswer();
+        }, child: Image.asset('images/ball$askAgainLaterAnswer.png'),
       ),
     );
   }
